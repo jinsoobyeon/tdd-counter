82cdb3b4c9a10bff6ddbde16f6a641be833d9e85
jinsoobyeon <jinsoobyeon@gmail.com>
2022-04-06 00:05:11 +0900

    Feat: 숫자 증가 및 감소 버튼

--- src/TDDCounter.tsx
+++ src/TDDCounter.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import { Container, Typography, Button, ButtonGroup } from "@mui/material";
+import { plus, minus } from "./count";
 
 function TDDCounter() {
+  const [number, setNumber] = useState<number>(0);
+
+  const increase = () => {
+    setNumber(plus(number));
+  };
+
+  const decrease = () => {
+    setNumber(minus(number));
+  };
+
   return (
     <Container maxWidth="sm" sx={{ textAlign: "center" }}>
       <Typography variant="h1" gutterBottom>
@@ -11,11 +23,11 @@ function TDDCounter() {
         aria-label="outlined primary button group"
         fullWidth
       >
-        <Button>puls</Button>
-        <Button>minus</Button>
+        <Button onClick={increase}>puls</Button>
+        <Button onClick={decrease}>minus</Button>
       </ButtonGroup>
       <Typography variant="h2" mt={2}>
-        0
+        {number}
       </Typography>
     </Container>
   );
